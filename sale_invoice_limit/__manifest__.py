835f3e58e2c6ba35831ec9f838955bb675c259c0
codexur88 <rebwar.good.luck@gmail.com>
2026-08-03 17:24:09 +0300

    Add files via upload

--- sale_invoice_limit/__manifest__.py
+++ sale_invoice_limit/__manifest__.py
@@ -50,7 +50,7 @@ How it works
 Compatible with Odoo 18 Community and Enterprise, Odoo.sh and self-hosted
 deployments.
     """,
-    'author': 'Codexui',
+    'author': 'Eng. Yasir Habeeb',
     'website': '',
     'depends': ['sale', 'account'],
     'data': [
@@ -66,6 +66,4 @@ deployments.
     'installable': True,
     'application': False,
     'license': 'LGPL-3',
-    'price': 149.0,
-    'currency': 'EUR',
 }
